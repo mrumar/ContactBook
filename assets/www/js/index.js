3890ccf24b01bc86920d7a146305878b5788933a
mrumar <czaroffnica7@gmail.com>
2012-12-13 16:55:41 +0100

    fixed bug with phone length

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -186,7 +186,7 @@ ContactBook = function() {
     	 fields = ["phoneNumbers", "displayName"];
     	 
     	 // if phone number is empty or invalid, add contact without searching in db
-    	 if (person.phone.length < 9) {
+    	 if (!person.phone || person.phone.length < 9) {
     		 self.updateContact(null, person);
     		 return false;
     	 }
